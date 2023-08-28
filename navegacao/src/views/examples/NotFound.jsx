037ef37bff6510ccbb4f97d47cb8375617fe27b0
Ruan Santos Dias <sdias.ruan@gmail.com>
2023-08-28 12:40:49 -0300

    ...

---
+++ navegacao/src/views/examples/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const NotFound = props => (
+    <div className="NotFount">
+        <h1>404</h1>
+        <h2>Opssss....</h2>
+    </div>
+)
+
+export default NotFound
